6bdbcdd67a46d1ae2c3c58f9e10d1db29ddacc96
Greattony Oduche <greattony96@gmail.com>
2021-05-28 17:54:15 +0300

    Admin Setup

--- src/user_profile/models.py
+++ src/user_profile/models.py
@@ -32,7 +32,7 @@ class UserProfileManager(BaseUserManager):
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database Schema for UserProfile"""
-    firs_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=250, unique=True)
     is_active = models.BooleanField(default=True)
@@ -46,11 +46,11 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         """This will return the full name of the user"""
-        return "%s %s" % (self.firs_name, self.last_name)
+        return "%s %s" % (self.first_name, self.last_name)
 
     def get_short_name(self):
         """This will return only the user's first name"""
-        return self.firs_name
+        return self.first_name
 
     def __str__(self):
         """This will return a string representation of the user"""
